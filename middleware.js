8e63d8e0c6ed3d00a0957e57409679e15c157d50
dekel.hershko <dekel.hershko@toluna.com>
2022-03-01 23:17:54 +0200

    add logout, get curr user, return to original page

--- middleware.js
+++ middleware.js
@@ -2,6 +2,7 @@ module.exports.isLoggedIn = (req, res, next) => {
     if (req.isAuthenticated())
         next()
     else {
+        req.session.returnTo = req.originalUrl
         req.flash('error', 'You must log in to view this page')
         res.redirect('/login')
     }
--- routes/users.js
+++ routes/users.js
@@ -8,13 +8,17 @@ router.get('/register', (req, res) => {
     res.render('users/register')
 })
 
-router.post('/register', async (req, res) => {
+router.post('/register', async (req, res, next) => {
     try {
         const { email, username, password } = req.body
         const user = new User({ email, username })
         const registeredUser = await User.register(user, password)
-        req.flash('success', 'Successfully registered to Soccer Score!')
-        res.redirect('/teams')
+        req.logIn(registeredUser, (error) => {
+            if (error)
+                return next(error)
+            req.flash('success', 'Successfully registered to Soccer Score!')
+            res.redirect('/teams')
+        })
     }
     catch (error) {
         req.flash('error', error.message)
@@ -28,7 +32,15 @@ router.get('/login', (req, res) => {
 
 router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), (req, res) => {
     const { username } = req.body
+    const redirectUrl = req.session.returnTo || '/teams'
+    delete req.session.returnTo
     req.flash('success', `Great seeing you again, ${username}!`)
+    res.redirect(redirectUrl)
+})
+
+router.get('/logout', (req, res) => {
+    req.logOut()
+    req.flash('success', 'Until next time!')
     res.redirect('/teams')
 })
 
